ef5b74a60a2c3e88caf6018b10b4dc1e1b338231
Kang Ha Na <kanghana0124@kangnam.ac.kr>
2024-12-04 12:11:04 +0900

    Update 1_📅캘린더.py

--- keep-your-determination/pages/1_📅캘린더.py
+++ keep-your-determination/pages/1_📅캘린더.py
@@ -52,17 +52,17 @@ def logout():
         st.write('<script>window.location.reload()</script>', unsafe_allow_html=True)
 
 def login():
-    secrets_file = os.getenv("GOOGLE_CLIENT_SECRETS")
-    if secrets_file:
-        with open("client_secret.json", "w") as f:
-            f.write(secrets_file)
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            "client_secret.json",
-            scopes=['https://www.googleapis.com/auth/calendar']
-        )
-        creds = flow.run_console()
-        save_credentials_to_file(creds)
-        return creds
+    secrets_file_content = st.secrets["GOOGLE_CLIENT_SECRETS"]
+    with open("client_secret.json", "w") as f:
+        f.write(secrets_file_content)
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        "client_secret.json",
+        scopes=['https://www.googleapis.com/auth/calendar']
+    )
+    creds = flow.run_console()
+    save_credentials_to_file(creds)
+    return creds
+
     else:
         st.error("환경 변수 'GOOGLE_CLIENT_SECRETS'가 설정되지 않았습니다.")
         return None
